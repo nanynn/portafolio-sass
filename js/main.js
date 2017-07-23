f08d61b97671ebd86fa4a3bf0a69a64cf294e290
Anayn Pavez <nanyn.pavez@gmail.com>
2017-07-23 20:05:02 -0300

    tratando de cambiar el color del navbar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+window.addEventListener('scroll', function () {
+  const distanciaVertical = window.pageYOffset || document.documentElement.scrollTop,
+  header = document.getElementById('header');
+  
+  if (distanciaVertical > 60) {
+    header.classList.add("navbar-color");
+  } else {
+    header.classList.remove("navbar-color");
+  }
+});
+
+
+
 $(document).ready(function() {
 	$(".dropdown-button").dropdown({ 
 		hover: true });

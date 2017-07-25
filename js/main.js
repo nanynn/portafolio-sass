73b6bbf62dec647940c23793dfc2e95fbbb0269e
Anayn Pavez <nanyn.pavez@gmail.com>
2017-07-24 21:39:08 -0300

    correccion de detalles y errores en secciones

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@
 $(document).ready(function() {
 	$(window).scroll(function() {
         if ($("#menu").offset().top > 56) {
-            $("#menu").addClass(".navbar-color");
+            $("#menu").css("background-color", "#3c4858");
+            $("#menu").css("box-shadow", "0 0 0.3em #000");
         } else {
-            $("#menu").removeClass(".navbar-color");
+            $("#menu").removeClass(".navbar-one");
         }
       });
 	$(".dropdown-button").dropdown({ 
